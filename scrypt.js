a4427859e4070e93c62e660e782433eeaeae8cda
slava <sv8997608@gmail.com>
2023-03-05 00:24:13 +0100

    first commit

---
+++ scrypt.js
@@ -0,0 +1,61 @@
+// 1)Найти произведение элементов массива.
+
+// const array = [1,2,3,4,5];
+// let sum = 0;
+// for(let i =0;i<array.length;i++){
+//     sum = array[i]+sum;
+// }
+// console.log(sum);
+
+// 2) Найти сумму квадратов элементов массива.
+
+// const array = [1,2,3,4,5];
+// let squareSum = 0;
+// for(let i =0;i<array.length;i++){
+//     squareSum += array[i]**2;
+// }
+// console.log(squareSum);
+
+// 3)Найти максимальное значение в массиве.
+
+// const array = [1,3,7,4,5,6];
+// let maxValue = array[0];
+// for(let i =0;i<array.length;i++){
+//     if(array[i] >maxValue)
+//         maxValue=array[i];
+    
+// }
+// console.log(maxValue);
+
+// 4)Найти  минимальное значение в массиве.
+
+// const array = [1,3,7,4,5,6];
+// let maxValue = array[0];
+// for(let i =0;i<array.length;i++){
+//     if(array[i] <maxValue)
+//         maxValue=array[i];
+    
+// }
+// console.log(maxValue);
+
+// 5)Найти среднее значение элементов массива.
+
+// const array = [1,3,7,4,5,6];
+// let sum = 0;
+// let result;
+// for(let i =0;i<array.length;i++){
+//     sum = array[i]+sum;
+//     result+=sum/array.length;
+    
+// }
+// console.log(result);
+
+// 6)Посчитать количество положительных элементов в массиве.
+
+// const array = [1,2,-4,-6,5,-7,6];
+// let result =0;
+// for(let i =0;i< array.length;i++){
+//     if(array[i]>0)
+//     result ++;
+// }
+// console.log(result)
